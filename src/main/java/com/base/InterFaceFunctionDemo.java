c6815caf589ea1566d31d17d35e57e19630d6cb0
Zhang Liangliang <zhangll@homewell.com.cn>
2020-01-03 11:33:46 +0800

    '添加函数式接口'

--- src/main/java/com/base/InterFaceFunctionDemo.java
+++ src/main/java/com/base/InterFaceFunctionDemo.java
@@ -1,12 +1,21 @@
 package com.base;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.function.Consumer;
+
 @FunctionalInterface
-interface FunctionA<K>{
-    K acc(K k);
+interface FunctionA<K> {
+    void acc(K k);
 }
 
 public class InterFaceFunctionDemo {
     public static void main(String[] args) {
-
+        FunctionA<String> functionA = a -> System.out.println(a);
+        functionA.acc("efa");
+//        Collections.unmodifiableList(new ArrayList(){{
+//            add("abc");
+//            add("cde");
+//        }}).forEach(functionA);
     }
 }
