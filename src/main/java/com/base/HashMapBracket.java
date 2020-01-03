34ccd962bb1bd082d2615b4b323ca4febfcbecf7
Zhang Liangliang <zhangll@homewell.com.cn>
2020-01-03 11:23:13 +0800

    '添加迭代器对象，并理解lambda表达式的不可修改外部变量的特性'

--- src/main/java/com/base/HashMapBracket.java
+++ src/main/java/com/base/HashMapBracket.java
@@ -17,11 +17,13 @@ public class HashMapBracket {
                 System.out.println("key:" + s + ",value:" + s2);
             }
         };
+        int a = 1;
         hashMap.forEach(biConsumer);
         System.out.println("方式2");
         // 方式2
         hashMap.forEach((key, value ) -> {
             System.out.println("key:" + key + ",value:" + value);
+//            a = 2; .// 报错，说明表达式不能够访问外部变量
         });
     }
 }
---
+++ src/main/java/com/base/InterFaceFunctionDemo.java
@@ -0,0 +1,12 @@
+package com.base;
+
+@FunctionalInterface
+interface FunctionA<K>{
+    K acc(K k);
+}
+
+public class InterFaceFunctionDemo {
+    public static void main(String[] args) {
+
+    }
+}
