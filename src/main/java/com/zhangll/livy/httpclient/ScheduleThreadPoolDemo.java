c1d92c5b56be652a2a406be5353f6133a45a725a
Zhang Liangliang <zhangll@homewell.com.cn>
2020-01-13 15:54:17 +0800

    schedule线程池的时候，可以定时在某个时间点进行任务的执行

--- src/main/java/com/zhangll/livy/httpclient/ScheduleThreadPoolDemo.java
+++ src/main/java/com/zhangll/livy/httpclient/ScheduleThreadPoolDemo.java
@@ -6,6 +6,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 public class ScheduleThreadPoolDemo {
     static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -17,24 +18,33 @@ public class ScheduleThreadPoolDemo {
                     public Thread newThread(Runnable r) {
                         Thread thread = new Thread(r, "HttpClient -");
                         System.out.println("factory: "+ Thread.currentThread() + Thread.currentThread().hashCode() + "      "+ sdf.format(new Date()));
-                        thread.setDaemon(true);
+//                        thread.setDaemon(true);
                         return thread;
                     }
                 });
 
 
         System.out.println(sdf.format(new Date()));
-        scheduledExecutorService.schedule(() -> {
-            System.out.println("name: " + Thread.currentThread().hashCode() + "      "+ sdf.format(new Date()));
-        }, 1, TimeUnit.SECONDS);
-        scheduledExecutorService.schedule(() -> {
-            System.out.println("name: " + Thread.currentThread().hashCode() + "      "+ sdf.format(new Date()));
-        }, 2, TimeUnit.SECONDS);
+
+        IntStream.rangeClosed(0,10)
+                .forEach(i -> {
+                    scheduledExecutorService.schedule(() -> {
+                        System.out.println("name: " + Thread.currentThread().hashCode() + "      "+ sdf.format(new Date()));
+                    }, i + 1, TimeUnit.SECONDS);
+                });
 
         // 提交任务
         System.out.println("1111");
         try {
-            TimeUnit.SECONDS.sleep(10);
+            while (true){
+                TimeUnit.SECONDS.sleep(10);
+                IntStream.rangeClosed(0,10)
+                        .forEach(i -> {
+                            scheduledExecutorService.schedule(() -> {
+                                System.out.println("name: " + Thread.currentThread().hashCode() + "      "+ sdf.format(new Date()));
+                            }, i + 1, TimeUnit.SECONDS);
+                        });
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- src/main/java/com/zhangll/web/URIDemo.java
+++ src/main/java/com/zhangll/web/URIDemo.java
@@ -5,7 +5,8 @@ import java.net.URISyntaxException;
 
 public class URIDemo {
     public static void main(String[] args) throws URISyntaxException {
-        URI uri = new URI("http://192.168.10.63");
+        URI uri = new URI("http://192.168.10.63/abl/casd");
         System.out.println(uri.getPort());
+        System.out.println(uri.getPath());
     }
 }
